1a417123163e34ba65240d862bce1cd9b1d9ac55
TopSteely <tobias.stahl1@gmx.de>
2014-12-01 19:15:11 +0100

    fixed overlap bug

--- Generic counting/overlap.m
+++ Generic counting/overlap.m
@@ -1,4 +1,4 @@
-function [intersection, p] = overlap(A, B, string)
+function [intersection, p] = overlap(im, A, B, string)
     left = max(A(1), B(1));
     right = min(A(1)+A(3), B(1)+B(3));
     bottom = max(A(2), B(2));
@@ -8,15 +8,7 @@ function [intersection, p] = overlap(A, B, string)
     surface_ground_truth = A(3)  * A(4);
     % round to 0.1 decimal
     p = round(surface_intersection / surface_ground_truth* 10)/10;
-    if any(intersection<0)
-        p = 0;
-    end
-    if p > 1
-        'bla'
-    end
     switch string
-        case 'partial'
-            p = p;
         case 'complete'
             if p > 0
                 p = 1;
@@ -25,9 +17,18 @@ function [intersection, p] = overlap(A, B, string)
             if p > 0.5
                 p = 1;
             end
-        otherwise
-            'wrong string'
+    end
+    if any(intersection<0)
+        imshow(im)
+        rectangle('Position', A, 'edgecolor', 'r', 'Linewidth', 3)
+        rectangle('Position', B, 'edgecolor', 'g', 'Linewidth', 3)
+        %rectangle('Position', int, 'edgecolor', 'b', 'Linewidth', 1.5)            
+        p = 0;
+    end
     if p < 0
         p = 0;
+    end
+    if p>1
+        'whaaaaaat??????'
     end
 end
